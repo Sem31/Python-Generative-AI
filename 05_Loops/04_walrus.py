5b80a177c63fe6abf914a05a4306d14ee35fcc4e
Sem31 <31kamleshprajapat@gmail.com>
2025-12-22 00:37:40 +0530

    Walrus operator

---
+++ 05_Loops/04_walrus.py
@@ -0,0 +1,11 @@
+# value = 13
+# remainder = value % 5
+
+# if remainder:
+#     print(f"Not divisible, remainder is {remainder}")
+
+
+value = 13
+
+if reminder := value % 5:
+    print(f"{value} is odd, remainder is {reminder}")
