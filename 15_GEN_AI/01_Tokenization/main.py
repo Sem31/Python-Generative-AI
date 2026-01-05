72716cbe4c862446ebddf85033ec7e73e13de726
Sem31 <31kamleshprajapat@gmail.com>
2026-01-06 02:24:39 +0530

    Information

--- 15_GEN_AI/01_Tokenization/main.py
+++ 15_GEN_AI/01_Tokenization/main.py
@@ -11,3 +11,10 @@ print("Encoded Tokens:", tokens)
 decoded = encoder.decode(tokens)
 print("Decoded Text:", decoded)
 # Decoded Text: Hey There! How are you doing today?
+
+
+# Steps:
+#     Step 1: Tokenization -> The process of converting text into smaller units called tokens, which can be words, subwords, or characters, making it easier for models to process and understand the text.
+#     Step 2: Vector Embeddings -> Transforming tokens into numerical vectors that capture semantic meaning, allowing the model to work with the text in a mathematical space.
+#     Step 3: Positional Encoding ->  Adding information to token embeddings to indicate the position of each token in the sequence, helping the model understand the order of words.
+#     Step 4: Self Attention Mechanism -> A method that allows the model to focus on different parts of the input sequence when generating each part of the output, improving context understanding.
